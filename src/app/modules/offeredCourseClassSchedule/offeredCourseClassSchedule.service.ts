ed57be31fd439d48707479d72eb81b2e07b998c5
anikdev2016 <anikdev2016@gmail.com>
2023-09-30 22:42:28 +0600

    refactored the schedule availability checker

--- src/app/modules/offeredCourseClassSchedule/offeredCourseClassSchedule.service.ts
+++ src/app/modules/offeredCourseClassSchedule/offeredCourseClassSchedule.service.ts
@@ -1,35 +1,11 @@
 import { OfferedCourseClassSchedule } from '@prisma/client';
-import httpStatus from 'http-status';
-import ApiError from '../../../errors/ApiError';
 import prisma from '../../../server';
-import { hasTimeConflict } from '../../../shared/utils';
+import { OfferedCourseClassScheduleUtils } from './offeredCourseClassSchedule.utils';
 
 const insertIntoDB = async (
   data: OfferedCourseClassSchedule
 ): Promise<OfferedCourseClassSchedule> => {
-  const doesClassScheduleExist =
-    await prisma.offeredCourseClassSchedule.findMany({
-      where: {
-        dayOfWeek: data.dayOfWeek,
-        roomId: data.roomId,
-      },
-    });
-  const existingSchedules = doesClassScheduleExist.map(item => ({
-    startTime: item.startTime,
-    endTime: item.endTime,
-    dayOfWeek: item.dayOfWeek,
-  }));
-  const newSlot = {
-    startTime: data.startTime,
-    endTime: data.endTime,
-    dayOfWeek: data.dayOfWeek,
-  };
-  if (hasTimeConflict(existingSchedules, newSlot)) {
-    throw new ApiError(
-      httpStatus.CONFLICT,
-      'Room is already booked for this schedule'
-    );
-  }
+  await OfferedCourseClassScheduleUtils.checkRoomAvailability(data);
   const result = await prisma.offeredCourseClassSchedule.create({
     data: data,
     include: {
